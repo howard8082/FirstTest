193acc03145bce49e4d7e4e9a8e3dae12230936d
howard8082 <howard8082@users.noreply.github.com>
2015-01-07 14:28:12 +0800

    开始treegird
    暂时扩展grid控件

---
+++ js/sta-treegrid.js
@@ -0,0 +1,49 @@
+﻿/**
+ * TreeGrid控件
+ * 依赖: jqueryui
+ *          core.js; widget.js
+ *       sta-grid.js
+ *
+ * @author zhuhao
+ */
+(function ( factory ) {
+    "use strict";
+    if ( typeof define === 'function' && define.amd )
+    {
+        define( ['jquery'], factory );
+    }
+    else
+    {
+        factory( jQuery );
+    }
+}(function ( $ ) {
+    "use strict";
+    return $.widget(
+        "sta.treegrid",
+        $.sta.grid,
+        {
+            options : {
+                height : '100px'
+            },
+            
+            _role : {
+                gridBody : 'treegrid'
+            },
+            
+            ///#region 私有方法
+            _create : function () {
+                // treegrid不给出最小行数，因为比较难算出具体行数。可以给出默认高度，最好是每行的默认高度? TODO
+                this.options.rowCount = -1;
+                this._super();
+            },
+            
+            _fnDrawEmptyGrid : function () {
+                //alert( this.options.sss );
+                this._super();
+                var innerGrid = this.element.find( "div[role='" + this._role.gridBody + "']" );
+                innerGrid.find( "table" ).removeClass( "dataTable" );
+            }
+            ///#endregion 私有方法
+        }
+    );
+}))
